89439811d7c4d557610177ad99fc2c2004030c70
Goran Ponjevic <45714339+minnuss@users.noreply.github.com>
2020-12-06 12:51:46 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.type', {
+    // Waits 1000ms after typing "First"
+    strings: ['Professional Music', 'Mix and Master', 'Audio Recording', 'World Class Production'],
+    typeSpeed: 100,
+    backSpeed: 70,
+    loop: true,
+});
